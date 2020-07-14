8ed977d15d197d10f33b6ead7e33e5ab0d9877a3
Antony <fvantop@gmail.com>
2020-07-14 20:25:53 +0300

    14/07

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into message values(3, 'Сходите в национльную библиотеку и покушайте драников', 'Минск');
-insert into message values(4, 'Город-герой, очень много мест ','Краснодар');
-insert into message values(5, 'Обязательно побывайте в главном театре','Новгород');
-insert into message values(6, 'Яркий, прекрасный и ночный город','Киев');
-insert into message values(7, 'Главная площадь очень величественна и велика','Гродно');
-insert into message values(8, 'Статуя свободы и много чего еще','Нью-Йорк');
-insert into message values(9, 'Поехал туда просто так','Стамбул');
+insert into message values(1, 'Go to the national library and eat potato pancakes', 'Minsk');
+insert into message values(2, 'Hero City, a lot of places','Krasnodar');
+insert into message values(3, 'Be sure to visit the main theater','Novgorod');
+insert into message values(4, 'Bright, beautiful and night city','Kiev');
+insert into message values(5, 'The main square is very majestic and large','Grodno');
+insert into message values(6, 'Statue of Liberty and much more','New-York');
+insert into message values(7, 'I went there just like that','Istanbul');
