671d538c73aca01d92d78c7b95dcac431e83c653
Antony <fvantop@gmail.com>
2020-07-15 13:39:55 +0300

    14/07

--- src/main/java/payroll/web/controller/MessageController.java
+++ src/main/java/payroll/web/controller/MessageController.java
@@ -3,10 +3,8 @@ package payroll.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import payroll.web.dao.repository.MessageRepository;
 import payroll.web.dao.service.MessageService;
 
 @Controller
